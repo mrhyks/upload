d1aa8979330cbfbe3d292e1053a1d5a0811eebb5
mrhyks <78847703+mrhyks@users.noreply.github.com>
2021-02-23 12:17:46 +0100

    Add files via upload

---
+++ thematrixhasyou.bat
@@ -0,0 +1,233 @@
+@ echo off
+title Enter the Matrix, Neo . . .
+color a
+echo W
+ping localhost -n .5 >nul
+cls
+echo Wa
+ping localhost -n 1 >nul
+cls
+echo Wak
+ping localhost -n 1 >nul
+cls
+echo Wake
+ping localhost -n 1 >nul
+cls
+echo Wake 
+ping localhost -n 1 >nul
+cls
+echo Wake u
+ping localhost -n 1 >nul
+cls
+echo Wake up
+ping localhost -n 1 >nul
+cls
+echo Wake up
+ping localhost -n 1 >nul
+cls
+echo Wake up,
+ping localhost -n 1 >nul
+cls
+echo Wake up, 
+ping localhost -n 1 >nul
+cls
+echo Wake up, N
+ping localhost -n 1 >nul
+cls
+echo Wake up, Ne
+ping localhost -n 1 >nul
+cls
+echo Wake up, Neo
+ping localhost -n 1 >nul
+cls
+echo Wake up, Neo.
+ping localhost -n 1 >nul
+cls
+echo Wake up, Neo..
+ping localhost -n 1 >nul
+cls
+echo Wake up, Neo...
+ping localhost -n 3 >nul
+cls
+echo T
+ping localhost -n 1 >nul
+cls
+echo Th
+ping localhost -n 1 >nul
+cls
+echo The
+ping localhost -n 1 >nul
+cls
+echo The M
+ping localhost -n 1 >nul
+cls
+echo The Ma
+ping localhost -n 1 >nul
+cls
+echo The Mat
+ping localhost -n 1 >nul
+cls
+echo The Matr
+ping localhost -n 1 >nul
+cls
+echo The Matri
+ping localhost -n .5 >nul
+cls
+echo The Matrix
+ping localhost -n .2 >nul
+cls
+echo The Matrix h
+ping localhost -n .2 >nul
+cls
+echo The Matrix ha
+ping localhost -n .2 >nul
+cls
+echo The Matrix has
+ping localhost -n .2 >nul
+cls
+echo The Matrix has y
+ping localhost -n .5 >nul
+cls
+echo The Matrix has yo
+ping localhost -n .5 >nul
+cls
+echo The Matrix has you
+ping localhost -n .5 >nul
+cls
+echo The Matrix has you.
+ping localhost -n .5 >nul
+cls
+echo The Matrix has you..
+ping localhost -n .4 >nul
+cls
+echo The Matrix has you...
+ping localhost -n 3 >nul
+cls
+echo F
+ping localhost -n .5 >nul
+cls
+echo Fo
+ping localhost -n .5 >nul
+cls
+echo Fol
+ping localhost -n .5 >nul
+cls
+echo Foll
+ping localhost -n .5 >nul
+cls
+echo Follo
+ping localhost -n .5 >nul
+cls
+echo Follow
+ping localhost -n .5 >nul
+cls
+echo Follow 
+ping localhost -n .5 >nul
+cls
+echo Follow t
+ping localhost -n .5 >nul
+cls
+echo Follow th
+ping localhost -n .5 >nul
+cls
+echo Follow the
+ping localhost -n .5 >nul
+cls
+echo Follow the 
+ping localhost -n .5 >nul
+cls
+echo Follow the w
+ping localhost -n .5 >nul
+cls
+echo Follow the wh
+ping localhost -n .5 >nul
+cls
+echo Follow the whi
+ping localhost -n .5 >nul
+cls
+echo Follow the whit
+ping localhost -n .5 >nul
+cls
+echo Follow the white
+ping localhost -n .5 >nul
+cls
+echo Follow the white 
+ping localhost -n .5 >nul
+cls
+echo Follow the white r
+ping localhost -n .5 >nul
+cls
+echo Follow the white ra
+ping localhost -n .5 >nul
+cls
+echo Follow the white rab
+ping localhost -n .5 >nul
+cls
+echo Follow the white rabb
+ping localhost -n .5 >nul
+cls
+echo Follow the white rabbi
+ping localhost -n .5 >nul
+cls
+echo Follow the white rabbit
+ping localhost -n .5 >nul
+cls
+echo Follow the white rabbit.
+ping localhost -n 4 >nul
+cls
+echo k
+ping localhost -n .5 >nul
+cls
+echo Kn
+cls
+echo Kno
+ping localhost -n .5 >nul
+cls
+echo Knoc
+ping localhost -n .5 >nul cls
+cls
+echo Knock
+ping localhost -n .5 >nul
+cls
+echo Knock,
+ping localhost -n .5 >nul
+cls
+echo Knock, 
+ping localhost -n .5 >nul
+cls
+echo Knock, k
+ping localhost -n .5 >nul
+cls
+echo Knock, kn
+ping localhost -n .5 >nul
+cls
+echo Knock, kno
+ping localhost -n .5 >nul
+cls
+echo Knock, knoc
+ping localhost -n .5 >nul
+cls
+echo Knock, knock,
+ping localhost -n .5 >nul
+cls
+echo Knock, knock, 
+ping localhost -n .5 >nul
+cls
+echo Knock, knock, N
+ping localhost -n .5 >nul
+cls
+echo Knock, knock, Ne
+ping localhost -n .5 >nul
+cls
+echo Knock, knock, Neo
+ping localhost -n .5 >nul
+cls
+echo Knock, knock, Neo.
+ping localhost -n 7 >nul
+
+@echo off
+title Enter the Matrix, Neo . . .
+color 0A
+:Matrix
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto Matrix
